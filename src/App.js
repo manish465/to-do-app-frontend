ed8f3086babda554ae2816d75bd5e20d6cac18a5
manish <ms5978307@gmail.com>
2023-04-05 14:20:50 +0530

    refactoring......

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { pageList } from "./pages";
 import AppContext from "./context/AppContext";
-import AppStructure from "./AppStructure";
-import ProtectedRoute from "./ProtectedRoute";
+import AppStructure from "./components/AppStructure";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 const App = () => {
     return (
--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { Navigate } from "react-router-dom";
-import { appContext } from "./context/AppContext";
+import { appContext } from "../context/AppContext";
 
 const ProtectedRoute = ({ children }) => {
     const { isAuthenticated } = useContext(appContext);
