e2d3e1e868261df207e53565f9f04d9fff87297a
manish <ms5978307@gmail.com>
2023-04-11 13:45:55 +0530

    removed strict mode

--- src/index.js
+++ src/index.js
@@ -3,8 +3,4 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-    <StrictMode>
-        <App />
-    </StrictMode>
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
